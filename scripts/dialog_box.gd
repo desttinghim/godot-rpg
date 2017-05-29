196d966b08ff4cdbff2f3377e88a8fb5d1fee67b
Louis Pearson <desttinghimgame@gmail.com>
2017-05-29 14:56:03 -0500

    Add initial decisions implementatino to dialog boxes

--- scripts/dialog_box.gd
+++ scripts/dialog_box.gd
@@ -2,37 +2,64 @@ extends CanvasLayer
 
 signal complete
 
-export var dialog = ["default"]
-export var page = 0
+enum PAGE_TYPE {
+	dialog,
+	decision
+}
+class Page:
+	var type = PAGE_TYPE.dialog
+	var dialog = ""
+	var decisions = []
+	func _init(type, dialog, decisions):
+		self.type = type
+		self.dialog = dialog
+		self.decisions = decisions
+
+var pages = []
+var page = 0
 
 var text = null
 var next = null
+var decisions = null
 
 func _ready():
 	text = get_node("screen/text")
 	next = get_node("screen/next")
+	decisions = get_node("screen/decisions")
 	next.connect("pressed", self, "next")
 	connect("complete", self, "test_signal_complete")
 	set_process_input(true)
 
 func add_dialog(line):
-	dialog.append(line)
+	pages.append(Page.new(PAGE_TYPE.dialog, line, "")) 
+
+func add_decision(options):
+	pages.append(Page.new(PAGE_TYPE.decision, "", options))
 
 func start():
 	load_page(page)
 	next.grab_focus()
 
 func load_page(pg):
-	if pg < dialog.size():
-		text.set_bbcode(dialog[pg])
+	if pg >= pages.size():
+		print("Error! No such page...")
+		return
+	if pages[pg].type == PAGE_TYPE.dialog:
+		text.set_bbcode(pages[pg].dialog)
+	else:
+		for option in pages[pg].decisions:
+			var btn = Button.new()
+			btn.set_text(option)
+			decisions.add_child(btn)
 	text.set_visible_characters(0)
 	next.hide()
 	text.grab_focus()
 
 func next():
 	page += 1
-	if page >= dialog.size():
+	if page >= pages.size():
 		emit_signal("complete")
+		queue_free()
 	else:
 		load_page(page)
 
