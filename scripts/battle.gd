9972e06ea02a6a5be38cffd9649dec34d6a0fb42
Louis Pearson <desttinghimgame@gmail.com>
2017-05-31 10:35:48 -0500

    Improve Moves and make focusing saner

--- scripts/battle.gd
+++ scripts/battle.gd
@@ -3,7 +3,6 @@ extends Node
 var Move = preload("res://scripts/move.gd")
 
 var battle_menu = null
-var battle_menu_focus = ""
 var sub_menu = null
 
 var combatants
@@ -11,24 +10,13 @@ var team_1
 var team_2
 var targeter
 
-class Action:
-	var origins = []
-	var targets = []
-	var stats = {"dmg":0, "cost":0}
-	func add_origin(node):
-		origins.append(node)
-	func add_target(node):
-		targets.append(node)
-	func set_stat(stat, val):
-		stats[stat] = val
-
-var main_menu = {moves = "showmenu_moves", items = "showmenu_items"}
+var main_menu = {Moves = "showmenu_moves", Items = "showmenu_items"}
 
 func _ready():
 	
 	battle_menu = get_node("interface/cntr_battle_menu/BattleMenu")
-	create_battle_menu(main_menu)
-	show_battle_menu()
+	var focus = create_battle_menu(main_menu)
+	show_battle_menu(focus)
 	
 	sub_menu = get_node("interface/cntr_battle_menu/SubMenu")
 	
@@ -40,89 +28,50 @@ func _ready():
 	set_process_input(true)
 
 # Callbacks
-var current_action
 func btn_callback(txt):
 	var call = txt.split("_")
 	if call[0] == "showmenu" and call.size() > 1:
 		if call[1] == "main":
-			create_battle_menu(main_menu)
-			show_battle_menu()
+			var focus = create_battle_menu(main_menu)
+			if call.size() > 2: focus = call[2]
+			show_battle_menu(focus)
 		elif call[1] == "moves":
-			create_battle_menu(get_moves_menu("player"))
-			show_battle_menu()
+			var focus = create_battle_menu(get_moves_menu("player"))
+			show_battle_menu(focus)
 		elif call[1] == "items":
 			pass # do nothing for now
 	print(txt)
 
-func get_moves_menu(node_name):
-	var node
-	for combatant in get_tree().get_nodes_in_group("combatant"):
-		if combatant.get_name() == node_name:
-			node = combatant
-			break
-	var moves = {}
-	for child in node.get_children():
-		print(child.get_type(), " : ", child.get_name())
-		if child extends Move:
-			moves[child.display_name] = str("move_", node.get_name(), "_", child.get_name())
-	moves["back"] = "showmenu_main" 
-	return moves
-
 func select_callback(target):
-	current_action.add_target(target)
-	hide_menu()
+	hide_battle_menu()
 	hide_target_select()
-	apply_action(current_action)
-
-var action_history = []
-func apply_action(action):
-	for origin in action.origins:
-		print(origin.get_name(), " uses ", action.stats["cost"], " mana.")
-	for target in action.targets:
-		print(target.get_name(), " takes ", action.stats["dmg"], " damage.")
-	action_history.append(action)
+	create_battle_menu(main_menu)
+	show_battle_menu()
 
 # Menu UI
 func create_battle_menu(menu):
 	for node in battle_menu.get_children():
 		node.queue_free()
+	var first = true
+	var focus
 	for key in menu.keys():
 		var btn = Button.new()
 		btn.text = key
+		btn.set_name(key)
 		btn.connect("pressed", self, "btn_callback", [menu[key]])
 		battle_menu.add_child(btn)
-		battle_menu_focus = btn.get_name()
-	#battle_menu_focus = battle_menu.get_child(0).get_name()
+		if first: 
+			first = false
+			focus = btn.get_name()
+	return focus
 
-func show_battle_menu():
+func show_battle_menu(focus):
 	battle_menu.show()
-	battle_menu.get_node(battle_menu_focus).grab_focus()
+	battle_menu.get_node(focus).grab_focus()
 
 func hide_battle_menu():
 	battle_menu.hide()
 
-# Sub menu UI
-func show_menu(menu):
-	battle_menu_focus = battle_menu.get_path_to(battle_menu.get_focus_owner())
-	for item in menu.keys():
-		var btn = Button.new()
-		btn.text = item 
-		btn.connect("pressed", self, "btn_callback", [menu[item]])
-		sub_menu.add_child(btn)
-	var back_btn = Button.new()
-	back_btn.text = "Back"
-	back_btn.connect("pressed", self, "hide_menu")
-	sub_menu.add_child(back_btn)
-	
-	sub_menu.show()
-	sub_menu.get_child(0).grab_focus()
-
-func hide_menu():
-	sub_menu.hide()
-	for node in sub_menu.get_children():
-		node.queue_free()
-	battle_menu.get_node(battle_menu_focus).grab_focus()
-	
 # Targeting UI
 func show_target_select(group):
 	for target in get_tree().get_nodes_in_group(group):
@@ -138,3 +87,18 @@ func hide_target_select():
 	for selector in targeter.get_children():
 		selector.queue_free()
 	targeter.hide()
+
+# Utilities
+func get_moves_menu(node_name):
+	var node
+	for combatant in get_tree().get_nodes_in_group("combatant"):
+		if combatant.get_name() == node_name:
+			node = combatant
+			break
+	var moves = {}
+	for child in node.get_children():
+		print(child.get_type(), " : ", child.get_name())
+		if child extends Move:
+			moves[child.display_name] = str("move_", node.get_name(), "_", child.get_name())
+	moves["Back"] = "showmenu_main_Moves" 
+	return moves
--- scripts/move.gd
+++ scripts/move.gd
@@ -2,6 +2,11 @@ extends Node
 
 export var display_name = "Move"
 export var cost = 0
+const TARGET_TEAM = -1
+const TARGET_ALL = -2
+export var targets = 1
+# turn on friendly
+export(String, "Enemies", "Friends", "Both") var can_target = "Enemies"
 export(String, MULTILINE) var _stats = ""
 var stats = {}
 
